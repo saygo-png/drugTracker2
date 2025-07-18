5f702387d75be84fe52bb6783432c262385dec17
saygo-png <saygo.mail@proton.me>
2025-07-18 19:19:04 +0200

    feat: fancier build without haddock
    Signed-off-by: saygo-png <saygo.mail@proton.me>

--- package.nix
+++ package.nix
@@ -1,14 +1,32 @@
 {pkgs}: let
+  hl = pkgs.haskell.lib;
+
+  listSwitchFunc = [
+    {
+      switch = false;
+      function = hl.buildStrictly;
+    }
+    {
+      switch = true;
+      function = pkgs.haskellPackages.generateOptparseApplicativeCompletions ["drug"];
+    }
+  ];
+
+  onSwitchApplyFunc = set: object:
+    if set.switch
+    then set.function object
+    else object;
+
   pkg = pkgs.haskellPackages.developPackage {
-    root =
-      pkgs.nix-gitignore.gitignoreSourcePure
-      [
-        "dist-newstyle"
-        ".*#"
-        ".git"
-      ]
-      ./.;
+    root = ./.;
     returnShellEnv = false;
+
+    modifier = drv:
+      hl.overrideCabal drv (_: {
+        doHaddock = false;
+        enableDeadCodeElimination = true;
+        passthru.nixpkgs = pkgs;
+      });
   };
 in
-  pkgs.haskellPackages.generateOptparseApplicativeCompletions ["drug"] pkg
+  pkgs.lib.foldr onSwitchApplyFunc pkg listSwitchFunc
