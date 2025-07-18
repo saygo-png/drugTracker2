4fb6f058a23a10a7cc98aedcec38a73e69ff8a64
saygo-png <saygo.mail@proton.me>
2025-07-18 18:28:35 +0200

    fix: formatter config
    Signed-off-by: saygo-png <saygo.mail@proton.me>

--- treefmt.nix
+++ treefmt.nix
@@ -6,17 +6,24 @@
   projectRootFile = "flake.nix";
 
   programs.alejandra.enable = true;
-
   programs.deadnix.enable = true;
-
   programs.statix.enable = true;
-
   programs.fourmolu.enable = true;
 
   settings.formatter = {
-    formoulu = {
-      options = ["--indentation 2" "--respectful false"];
+    fourmolu = {
+      options = [
+        "--indentation"
+        "2"
+
+        "--respectful"
+        "false"
+
+        "--indent-wheres"
+        "true"
+      ];
     };
+
     trim-newlines = {
       command = "${lib.getExe pkgs.gnused}";
       options = lib.strings.splitString " " ''-i -e :a -e /^\n*$/{$d;N;ba -e }'';
