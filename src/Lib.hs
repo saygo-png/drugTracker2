ec5ee9219b8172cae714c51d938c1f6b27916d2b
saygo-png <saygo.mail@proton.me>
2025-07-27 08:17:55 +0200

    feat: cut off milliseconds
    Signed-off-by: saygo-png <saygo.mail@proton.me>

--- src/Lib.hs
+++ src/Lib.hs
@@ -9,4 +9,5 @@ import Data.Time (getCurrentTimeZone, utcToLocalTime)
 toPrettyLocalTime :: UTCTime -> IO Text
 toPrettyLocalTime utcTime = do
   localTZ <- getCurrentTimeZone
-  utcToLocalTime localTZ utcTime & tshow & takeWhile (/= '.') & pure
+  let text = utcToLocalTime localTZ utcTime & tshow
+  takeWhile (/= '.') text & dropEnd 3 & pure
