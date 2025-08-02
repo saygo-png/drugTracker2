1d32624243bc189e7151acc61dba7101b498e1f8
saygo-png <saygo.mail@proton.me>
2025-08-02 17:39:10 +0200

    f
    Signed-off-by: saygo-png <saygo.mail@proton.me>

--- nixos.nix
+++ nixos.nix
@@ -5,34 +5,37 @@
   ...
 }: {
   options = {
-    drugtracker2.enable = lib.mkEnableOption "drugtracker2";
+    programs.drugtracker2.enable = lib.mkEnableOption "drugtracker2";
   };
 
-  environment.systemPackages = [
-    (pkgs.callPackage ./package.nix {})
-  ];
+  config = let
+    drug = pkgs.callPackage ./package.nix {};
+  in
+    lib.mkIf config.drugtracker2.enable
+    {
+      environment.systemPackages = [drug];
 
-  config = lib.mkIf config.drugtracker2.enable {
-    systemd.user.services.drug-reminder = {
-      description = "Drug Reminder Service";
-      serviceConfig = {
-        Type = "oneshot";
-        ExecStart = let
-          bash = "${pkgs.bash}/bin/bash";
-          notify-send = "${pkgs.libnotify}/bin/notify-send";
-        in "${bash} -c 'drug remind || ${notify-send} \"Drug Reminder\" \"Time to take your drugs!\"'";
-        Environment = "DISPLAY=:0";
+      systemd.user.services.drug-reminder = {
+        description = "Drug Reminder Service";
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart = let
+            drugBin = "${drug}/bin/drug";
+            bash = "${pkgs.bash}/bin/bash";
+            notify-send = "${pkgs.libnotify}/bin/notify-send";
+          in "${bash} -c '${drugBin} remind || ${notify-send} \"Drug Reminder\" \"Time to take your drugs!\"'";
+          Environment = "DISPLAY=:0";
+        };
+      };
+
+      systemd.user.timers.drug-reminder = {
+        description = "Run Drug Reminder Every Hour";
+        wantedBy = ["timers.target"];
+        timerConfig = {
+          OnCalendar = "hourly";
+          Persistent = true;
+          RandomizedDelaySec = 60;
+        };
       };
     };
-
-    systemd.user.timers.drug-reminder = {
-      description = "Run Drug Reminder Every Hour";
-      wantedBy = ["timers.target"];
-      timerConfig = {
-        OnCalendar = "hourly";
-        Persistent = true;
-        RandomizedDelaySec = 60;
-      };
-    };
-  };
 }
