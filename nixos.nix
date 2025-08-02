19874a674b4f80c6fad6c2f59594ab1fddc01515
saygo-png <saygo.mail@proton.me>
2025-08-02 17:34:30 +0200

    f
    Signed-off-by: saygo-png <saygo.mail@proton.me>

--- nixos.nix
+++ nixos.nix
@@ -1,23 +1,38 @@
-{pkgs, ...}: {
-  systemd.user.services.drug-reminder = {
-    description = "Drug Reminder Service";
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = let
-        bash = "${pkgs.bash}/bin/bash";
-        notify-send = "${pkgs.libnotify}/bin/notify-send";
-      in "${bash} -c 'drug remind || ${notify-send} \"Drug Reminder\" \"Time to take your drugs!\"'";
-      Environment = "DISPLAY=:0";
-    };
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
+  options = {
+    drugtracker2.enable = lib.mkEnableOption "drugtracker2";
   };
 
-  systemd.user.timers.drug-reminder = {
-    description = "Run Drug Reminder Every Hour";
-    wantedBy = ["timers.target"];
-    timerConfig = {
-      OnCalendar = "hourly";
-      Persistent = true;
-      RandomizedDelaySec = 60;
+  environment.systemPackages = [
+    (pkgs.callPackage ./package.nix {})
+  ];
+
+  config = lib.mkIf config.drugtracker2.enable {
+    systemd.user.services.drug-reminder = {
+      description = "Drug Reminder Service";
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = let
+          bash = "${pkgs.bash}/bin/bash";
+          notify-send = "${pkgs.libnotify}/bin/notify-send";
+        in "${bash} -c 'drug remind || ${notify-send} \"Drug Reminder\" \"Time to take your drugs!\"'";
+        Environment = "DISPLAY=:0";
+      };
+    };
+
+    systemd.user.timers.drug-reminder = {
+      description = "Run Drug Reminder Every Hour";
+      wantedBy = ["timers.target"];
+      timerConfig = {
+        OnCalendar = "hourly";
+        Persistent = true;
+        RandomizedDelaySec = 60;
+      };
     };
   };
 }
